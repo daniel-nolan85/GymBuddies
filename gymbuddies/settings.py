a34feb1b53991cb77b432ff4a8b9a6d86ff32760
daniel-nolan85 <57992802+daniel-nolan85@users.noreply.github.com>
2021-02-05 19:54:57 +0000

    Update settings.py

--- gymbuddies/settings.py
+++ gymbuddies/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'dqz-tnwbuhtu0q=x79f@r&rt)967u%ufnbiu+$rr8ox8o4%a6i'
+SECRET_KEY = '*******'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
